374007ae7254fe5175361e8a9137450ab86cd7e4
erodriguez10218 <erica@windemere.us.com>
2016-02-24 15:37:19 -0600

    HTML and CSS

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> </title>    
+    <style>
+        body {
+            background-color: cornsilk;
+        }
+        #container {
+            width: 960px;
+            min-height: 600px; 
+            background: ivory;
+            color:aliceblue;
+            font-family: cursive;
+            font-size: 20px;
+        }
+        main {
+            min-height: 500px;
+            
+        }
+        
+        footer{
+            min-height: 50px;
+        }
+    </style>
+
+</head>
+    <body>
+    <div id="container">
+        <h1>Text should go here</h1>
+        <main>
+        
+        </main>
+        <footer>
+        
+        </footer>
+        </div>
+    </body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> </title>    
+    <style>
+        body {
+            background-color: cornsilk;
+        }
+        #container {
+            width: 960px;
+            min-height: 600px; 
+            background: darkolivegreen;
+            color:cornsilk;
+            font-family: sans-serif;
+            font-size: 20px;
+        }
+        main {
+            min-height: 500px;
+            
+        }
+        
+        footer{
+            min-height: 50px;
+        }
+    </style>
+
+</head>
+    <body>
+    <div id="container">
+        <h1>Text should go here</h1>
+        <main>
+        
+        </main>
+        <footer>
+            <?php include 'footer.php';?>
+        </footer>
+        </div>
+    </body>
+</html>
